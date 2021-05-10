d344b97b07b62225c4de3cc3af60b36a9d7578e2
xLexip <x@lexip.dev>
2021-05-10 16:12:45 +0200

    cheeseburger: Nuke build description

--- aosp_cheeseburger.mk
+++ aosp_cheeseburger.mk
@@ -47,6 +47,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Fingerprint
 BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210405.005 7181113 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210505.003 7255357 release-keys"
 
 TARGET_VENDOR := oneplus
