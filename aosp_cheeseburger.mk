8be2fbf4accba49bce701deb3029974ab93978c8
xLexip <x@lexip.dev>
2021-05-10 16:12:50 +0200

    cheeseburger: Update fingerprint to redfin may

--- aosp_cheeseburger.mk
+++ aosp_cheeseburger.mk
@@ -45,7 +45,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=OnePlus5
 
 # Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210505.003 7255357 release-keys"
 
